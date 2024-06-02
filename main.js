0180cf1c2f456d76efce5a770f22d01ec9326c8e
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-02 10:38:40 +0200

    {UPD} added javascript email list

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 `use strict`
 
+// aggiungo un data
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        title: `Prova Titolo`,
+        email: [],
+        // url del api
+        url: `https://flynn.boolean.careers/exercises/api/random/mail`
+      };
+    },
+    created(){
+      // ciclo di email
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.url).then((response) => {
+            this.email.push(response.data.response)
+            console.log(response.data.response);
+        })
+        
+      }
+        
+    },
+  }).mount('#app')
